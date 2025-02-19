edec7cd6d5af10b3d2c842474dd8ba3f8248778b
Yauheni Kiryienkau <ya@redstream.by>
2025-02-19 11:17:00 +0300

    added custom events

---
+++ src/Dto/CustomEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Taddy\Sdk\Dto;
+
+enum CustomEvent: string {
+    case Custom1 = 'custom1';
+    case Custom2 = 'custom2';
+    case Custom3 = 'custom3';
+    case Custom4 = 'custom4';
+}
